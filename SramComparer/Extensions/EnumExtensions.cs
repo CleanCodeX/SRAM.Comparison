ccde7db5130ece5586b30dde48fd4598aac33adf
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2020-10-07 18:01:16 +0200

    bugfix of bugfix

--- SramComparer/Extensions/EnumExtensions.cs
+++ SramComparer/Extensions/EnumExtensions.cs
@@ -5,7 +5,6 @@ namespace SramComparer.Extensions
 {
     public static class EnumExtensions
     {
-        public static string ToFlagsString<T>(this T source) where T: Enum => 
-            Equals((int)(object)source, 0) ? Resources.None : source.ToString();
+        public static string ToFlagsString(this Enum source) => source.ToString() == "0" ? Resources.None : source.ToString();
     }
 }
